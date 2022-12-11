93ed06e39487065dfe18ba3e8f583999f6fe84d1
SoheilMV <Soheil.8221@gmail.com>
2022-12-11 15:42:55 +0330

    Initial

---
+++ ControlDragMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace System.Windows.Forms
+{
+    public static class ControlDragMove
+    {
+        private static Point _mouseLocation;
+
+        public static void DragMove(this Control control)
+        {
+            control.MouseDown += Control_MouseDown;
+            control.MouseMove += Control_MouseMove;
+        }
+
+        private static void Control_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseLocation = new Point(e.X, e.Y);
+        }
+
+        private static void Control_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Control control = (Control)sender;
+                Form form = (Form)control.TopLevelControl;
+                form.Location = new Point(form.Location.X + (e.X - _mouseLocation.X), form.Location.Y + (e.Y - _mouseLocation.Y));
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# ControlDragMove
+Easy Drag & Move in Windows Forms.
+
+# How To Use
+1. Copy and paste **ControlDragMove.cs** into your project.
+2. Choose a name for the control you want to use.
+3. Use your control's DragMove method in your form's **load event** or **constructor**.
+4. Run your app and enjoy.
